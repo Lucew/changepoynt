57ca728651bd8b8a7359c5bc864f74013c06d1df
Lucas Weber <lucas@CS6.FAU.DE>
2023-03-09 15:54:56 +0100

    change setup.py to automatically read requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup
 from setuptools import find_packages
+from os import path
+
+
+# read the contents of requirements.txt
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+with open(path.join(this_directory, 'requirements_tests.txt'), encoding='utf-8') as f:
+    requirements_tests = f.read().splitlines()
 
 
 def main():
@@ -8,26 +17,22 @@ def main():
         name="changepoynt",
         version="0.0.1",
         author="Lucas Weber",
-        author_email="",
+        author_email="weber-lucas@web.de",
         url="",
         description="Readable package for several change point detection methods implemented in python.",
         long_description="This package contains several readable change point detections methods in python.",
         zip_safe=False,
         include_package_data=True,
-        packages=find_packages(),
-        install_requires=[
-            "numpy",
-            "scipy",
-            "numba",
-        ],
+        packages=find_packages(exclude=['tests']),
+        install_requires=requirements,
         dependency_links=[],
-        tests_require=[],
+        tests_require=requirements_tests,
         setup_requires=[],
-        license="MIT",
+        license="BSD License",
         test_suite="tests",
         classifiers=[
             "Programming Language :: Python :: 3.9",
-            "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+            "License :: OSI Approved :: BSD License",
             "Topic :: Scientific/Engineering",
         ]
     )
