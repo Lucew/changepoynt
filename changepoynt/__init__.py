3ffab671c5fdae44bc268f46e98bcc2902778bf0
Lucas Weber <lucas@CS6.FAU.DE>
2023-04-11 14:58:07 +0200

    package clean up

---
+++ changepoynt/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import algorithms
+from . import utils
+from . import visualization
+# TODO: add version information here
+
+__all__ = ['algorithms', 'utils', 'visualization']
