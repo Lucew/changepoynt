c5c404672124fe97bb52a2f5c2bd6c7bd1897d3a
Lucas Weber <lucas@CS6.FAU.DE>
2023-11-29 16:32:44 +0100

    delete circular imports

--- changepoynt/__init__.py
+++ changepoynt/__init__.py
@@ -1,8 +1,1 @@
-# -*- coding: utf-8 -*-
 
-from . import algorithms
-from . import utils
-from . import visualization
-# TODO: add version information here
-
-__all__ = ['algorithms', 'utils', 'visualization']
