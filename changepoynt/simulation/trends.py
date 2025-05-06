dff56c11c5dbf24deb8f1f990bcbc85e1164d361
Lucas <lucas.weber@fau.de>
2025-05-06 17:41:58 +0200

    add basic trends

--- changepoynt/simulation/trends.py
+++ changepoynt/simulation/trends.py
@@ -1,2 +1,43 @@
 # TODO: specify trends (linear, offset, maybe even signals)
 # TODO: take care the trends are always different to NoTrends(offset of zero)
+import numpy as np
+
+import base
+
+
+class ConstantOffset(base.ConstantTrend):
+    """
+    Adding a constant to the signal.
+    """
+    def __init__(self, offset: float, shape: tuple[int] | base.Signal):
+        super().__init__(offset, shape)
+        if offset == 0:
+            raise ValueError("Constant offset cannot be 0. Use NoTrend class instead.")
+
+
+class LinearTrend(base.BaseTrend):
+
+    def __init__(self, offset: float, slope: float, shape: tuple[int] | base.Signal):
+
+        # save the variables
+        self.shape_tuple = base.Signal.translate_shape(shape)
+        self.offset = offset
+        self.slope = slope
+
+    def render(self) -> np.ndarray:
+        return self.offset + self.slope * np.linspace(0, self.shape[0]-1, self.shape[0])
+
+    @property
+    def shape(self) -> tuple[int,]:
+        return self.shape_tuple
+
+    def __eq__(self, other):
+
+        # if the other one is also a trend, we have to check whether they would attach to each other
+        # without a problem
+        if isinstance(other, LinearTrend):
+            return self.offset + self.slope * (other.shape[0]-1) == other.offset and self.slope == other.slope
+
+
+
+
